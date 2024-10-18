d4c4aeaeaf677b09b083180691f1c834cdaed410
Youssef Ouarad <youssef.ouarad@polymtl.ca>
2024-10-18 10:11:59 -0400

    c

--- config/db.js
+++ config/db.js
@@ -1,21 +1,19 @@
-require("dotenv").config(); // Load environment variables from .env file
+require("dotenv").config(); 
 
 const { Sequelize } = require("sequelize");
 
-// Create a new Sequelize instance to connect to the PostgreSQL database
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 5432, // Explicitly set the port
+    port: process.env.DB_PORT || 5432,
     dialect: "postgres",
   }
 );
 
 
-// Test the connection to the PostgreSQL database
 sequelize
   .authenticate()
   .then(() => {
@@ -25,5 +23,4 @@ sequelize
     console.error("Unable to connect to the database:", err);
   });
 
-// Export the sequelize instance for use in other parts of the application
 module.exports = sequelize;
